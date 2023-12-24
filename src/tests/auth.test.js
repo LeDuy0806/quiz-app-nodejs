66d3448030632dd4ed538e707738c96b1ad003ae
QuocAnh189 <146860044+QuocAnh189@users.noreply.github.com>
2023-12-24 21:12:59 +0700

    Update auth test (#37)
    * update user
    
    * add AI model
    
    * edit small
    
    * InitTest
    
    * update test
    
    * update auth signUp

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -5,6 +5,10 @@ import {
     signUpUserNameExist,
     signInValid,
     signInEmailNotExist,
+    signInPassWordInvalid,
+    EmailFormat,
+    RequirePassword,
+    RequireLong,
     signInPassWordInvalid
 } from '../utilsTest/auth';
 import bcrypt from 'bcrypt';
@@ -29,6 +33,201 @@ describe('Auth Functionality', () => {
     });
 
     describe('user SignUp', () => {
+
+        describe('given the mail is format and the password is strong and userName is standard length', () => {
+            test('should return the true and strong and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com',
+                    password: 'QuocAnh-1809',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'strong', 'strong']);
+            });
+        });
+
+        describe('given the mail is format and the password is strong and userName is not standard length', () => {
+            test('should return the true and strong and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com',
+                    password: 'QuocAnh-1809',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'strong', 'weak']);
+            });
+        });
+
+        describe('given the mail is format and the password is medium and userName is standard length', () => {
+            test('should return the true and medium and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com',
+                    password: 'Quoc1809',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'medium', 'strong']);
+            });
+        });
+
+        describe('given the mail is format and the password is medium and userName is not standard length', () => {
+            test('should return the true and medium and weak and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com',
+                    password: 'Quoc1809',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'medium', 'weak']);
+            });
+        });
+
+        describe('given the mail is format and the password is weak and userName is standard length', () => {
+            test('should return the true and weak and strong and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com',
+                    password: 'Quoc189',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'weak', 'strong']);
+            });
+        });
+
+        describe('given the mail is format and the password is weak and userName is not standard length', () => {
+            test('should return the true and weak and strong and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail.com and',
+                    password: 'Quoc189',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([true, 'weak', 'weak']);
+            });
+        });
+
+        describe('given the mail is not format and the password is strong and userName is standard length', () => {
+            test('should return the false and weak and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'QuocAnh-1809',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'strong', 'strong']);
+            });
+        });
+
+        describe('given the mail is not format and the password is strong and userName is not standard length', () => {
+            test('should return the false and weak and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'QuocAnh-1809',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'strong', 'weak']);
+            });
+        });
+
+        describe('given the mail is not format and the password is medium and userName is standard length', () => {
+            test('should return the false and medium and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'Quoc1809',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'medium', 'strong']);
+            });
+        });
+
+        describe('given the mail is not format and the password is medium and userName is not standard length', () => {
+            test('should return the false and medium and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'Quoc1809',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'medium', 'weak']);
+            });
+        });
+
+        describe('given the mail is not format and the password is weak and userName is standard length', () => {
+            test('should return the false and weak and strong', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'Quoc189',
+                    userName: 'QuocAnh'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'weak', 'strong']);
+            });
+        });
+
+        describe('given the mail is not format and the password is weak and userName is not standard length', () => {
+            test('should return the false and weak and weak', async () => {
+                const user = {
+                    mail: 'anhquoc18092003@gmail',
+                    password: 'Quoc189',
+                    userName: 'Quoc'
+                };
+                expect([
+                    EmailFormat(user.mail),
+                    RequirePassword(user.password),
+                    RequireLong(user.userName)
+                ]).toEqual([false, 'weak', 'weak']);
+            });
+        });
+
+        // describe('given the SignUp information are valid', () => {
+        //     test('should return the user payload', async () => {
+        //         const { mail, userName } = signUpValid;
+        //         const users = db.collection('users');
+        //         const emailExits = await users.findOne({
+        //             mail
+        //         });
+        //         const userNameExist = await users.findOne({
+        //             userName
+        //         });
+        //         expect([emailExits, userNameExist]).toEqual([null, null]);
+        //     });
+        // });
+
         describe('given the SignUp information are valid', () => {
             test('should return the user payload', async () => {
                 const { mail, userName } = signUpValid;
